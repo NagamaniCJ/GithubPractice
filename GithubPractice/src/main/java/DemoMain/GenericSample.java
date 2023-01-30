0500cc00db222beacc1456eafbda499ef61cd2a2
Nikitha <Nikitha@LAPTOP-CG8D5202>
2023-01-30 17:59:59 +0530

    new class added should be commited

---
+++ GithubPractice/src/main/java/DemoMain/GenericSample.java
@@ -0,0 +1,11 @@
+package DemoMain;
+
+public class GenericSample {
+	
+	public void gSample()
+	{
+		System.out.println("this class is created to show the example for PULL operation");
+		System.out.println("PULL operation is to get newly added changes to global and from global to working directory ");
+	}
+
+}
